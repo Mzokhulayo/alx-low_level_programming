105b57a949e5eb71686e31e08c7e30e78e5785e2
Mzokhulayo <mzokhulayo.mdubeki@hotmail.com>
2023-08-16 00:55:18 +0200

    1st attempmt on alphabet problem

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,3 +1,17 @@
 #include <stdio.h>
 
+void print_alphabet()
+{
+char 1st_letter = 'a';
+while (1st_letter <='z') {
+	_putChar(1st_letter);
+	1st_letter ++;
+	}
+	_putChar('\n');
+  }
 
+int main(void) 
+{
+print_alphabet();
+return (0);
+}

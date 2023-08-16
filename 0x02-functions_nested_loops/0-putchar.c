935ab34eb0606e7da357ac35c2f92d85d7fcaa1e
Mzokhulayo <mzokhulayo.mdubeki@hotmail.com>
2023-08-17 00:42:43 +0200

    added _putchar main file

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include "main.h"
 
+
 /**
  * _putchar - Prints a character to stdout.
  * @c: The character to be printed.
@@ -11,24 +12,4 @@
 int _putchar(char c)
 {
 return (putchar(c));
-}
-
-/**
- * main - Entry point of the program.
- *
- * Return: Always 0 (Success).
- */
-int main(void)
-{
-
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-_putchar('\n');
-return (0);
 }

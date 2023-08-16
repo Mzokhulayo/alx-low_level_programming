0711f281afaa1e412cfec2ca72109d7a8044610b
Mzokhulayo <mzokhulayo.mdubeki@hotmail.com>
2023-08-16 22:03:40 +0200

    fixed alphabet method

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,19 +1,37 @@
 #include <stdio.h>
+#include <main.h>
 
-void print_alphabet(void);
-
-
-void print_alphabet()
+/**
+ * print_alphabet - Prints the English alphabet in lowercase.
+ *
+ * This function prints the 26 lowercase letters of the English alphabet
+ * in consecutive order to the standard output, separated by spaces.
+ * The output is displayed on a single line.
+ *
+ * Usage:
+ *   print_alphabet();
+ */
+void print_alphabet(void)
 {
 char first_letter = 'a';
-while (first_letter <='z') {
-	putchar(first_letter);
-	first_letter ++;
-	}
-	putchar('\n');
-  }
+while (first_letter <= 'z')
+{
+_putChar(first_letter);
+first_letter++;
+}
+_putChar('\n');
+}
 
-int main(void) 
+/**
+ * main - Entry point of the program.
+ *
+ * This function calls the `print_alphabet` function to print the
+ * English alphabet
+ * in lowercase and then returns 0 to indicate successful execution.
+ *
+ * Return: Always returns 0 upon successful completion.
+ */
+int main(void)
 {
 print_alphabet();
 return (0);

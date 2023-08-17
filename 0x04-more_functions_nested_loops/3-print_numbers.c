27359bbeddfab138b9cfd27bfd0e87f76a27a463
Mzokhulayo <mzokhulayo.mdubeki@hotmail.com>
2023-08-17 17:28:55 +0200

    excluded 2 and 4 from printing

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,22 +1,18 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
- * print_numbers - Prints the numbers from 0 to 9
+ * print_numbers - Prints the numbers from 0 to 9, followed by a new line
  *
  * Return: void
  */
-
 void print_numbers(void)
 {
-int i;
-for (i = 0; i <= 9; i++)
-{
-if (i != 2 && i != 4)
-{
-_putchar(i + '0');
-}
-}
-_putchar('\n');
-}
+    int i;
 
+    for (i = 0; i <= 9; i++)
+    {
+        _putchar(i + '0');
+    }
+
+    _putchar('\n');
+}

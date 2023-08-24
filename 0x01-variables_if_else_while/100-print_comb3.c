100a75a00bda557c614e5dc79586ec475ac83178
Mzokhulayo <mzokhulayo.mdubeki@hotmail.com>
2023-08-24 21:46:39 +0200

    100.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,28 +9,40 @@
  *
  * Return: Always 0 (success)
  */
-
 int main(void)
 {
 int i = 0;
 while (i < 100)
 {
+int j = i + 1;
+while (j < 100)
+{
 putchar(i / 10 + '0');
 putchar(i % 10 + '0');
+putchar(',');
+putchar(' ');
+putchar(j / 10 + '0');
+putchar(j % 10 + '0');
 
-if (i < 89)
+if (i < 98)
 {
-    putchar(',');
-    putchar(' ');
+putchar(',');
+putchar(' ');
 }
 
+j++;
+
+if (j % 10 == 0)
+j += 10 - 1;
+}
 i++;
 
 if (i % 10 == 0)
-    i += 10 - 1;
+i += 10 - 1;
 }
 
 putchar('\n');
 
 return (0);
 }
+

22dd50bcf899427160995cbc443b520f9f899d9b
Mzokhulayo <mzokhulayo.mdubeki@hotmail.com>
2023-08-24 21:42:08 +0200

    100.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * This program prints all possible different combinations of two digits.
+ * Numbers are separated by ", " and are printed in ascending order.
+ * Duplicates like 01, 02, ..., 09 are skipped.
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+int i = 0;
+while (i < 100)
+{
+putchar(i / 10 + '0');
+putchar(i % 10 + '0');
+
+if (i < 89)
+{
+    putchar(',');
+    putchar(' ');
+}
+
+i++;
+
+if (i % 10 == 0)
+    i += 10 - 1;
+}
+
+putchar('\n');
+
+return (0);
+}

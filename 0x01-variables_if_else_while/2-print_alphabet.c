add62db505c214c959376a0c69acb282cb95b811
Mzokhulayo <mzokhulayo.mdubeki@hotmail.com>
2023-08-24 20:57:42 +0200

    added and updated 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -5,13 +5,14 @@
  *
  * Return: Always 0 (Success)
  */
+ 
 int main(void)
 {
-	char c;
+char c;
 
-	for (c = 'a'; c <= 'z'; c++)
-		putchar(c);
+for (c = 'a'; c <= 'z'; c++)
+putchar(c);
 
-	putchar('\n');
-	return (0);
+putchar('\n');
+return (0);
 }

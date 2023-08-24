bd9fdb8658b92eecb592bf52b599066f721f1135
Mzokhulayo <mzokhulayo.mdubeki@hotmail.com>
2023-08-24 23:25:33 +0200

    updated 100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,28 +1,19 @@
+#include "myHeader.h"
 #include <stdio.h>
 
 char *rot13(char *input) {
     char *output = input;
 
     for (; *input != '\0'; input++) {
-        if ((*input >= 'A' && *input <= 'Z') || (*input >= 'a' && *input <= 'z')) {
-            if (*input >= 'A' && *input <= 'Z') {
-                *input = ((*input - 'A' + 13) % 26) + 'A';
-            } else if (*input >= 'a' && *input <= 'z') {
-                *input = ((*input - 'a' + 13) % 26) + 'a';
-            }
+        char original = *input;
+        char offset = (original >= 'A' && original <= 'Z') ? 'A' : (original >= 'a' && original <= 'z') ? 'a' : 0;
+        
+        if (offset != 0) {
+            char rotated = (original - offset + 13) % 26 + offset;
+            *input = rotated;
         }
     }
 
     return output;
-}
-
-int main() {
-    char text[] = "Hello, World!";
-    printf("Original: %s\n", text);
-
-    char *encoded = rot13(text);
-    printf("Encoded:  %s\n", encoded);
-
-    return 0;
 }
 

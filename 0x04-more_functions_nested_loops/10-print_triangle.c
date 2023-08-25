ea5671fef0acef640a36494527509b6e5452c2c1
Mzokhulayo <mzokhulayo.mdubeki@hotmail.com>
2023-08-25 19:22:13 +0200

    10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -8,21 +8,29 @@
  */
 void print_triangle(int size)
 {
-int row, col;
-if (size <= 0)
+int c, i, j;
+
+c = 0;
+i = size - 1;
+while (c < size)
 {
-_putchar('\n');
+i = size - 1 - c;
+j = c + 1;
+while (i > 0)
+{
+_putchar(' ');
+i--;
 }
-else
-{
-for (row = 1; row <= size; row++)
-{
-for (col = 1; col <= row; col++)
+while (j > 0)
 {
 _putchar('#');
+j--;
 }
 _putchar('\n');
+c++;
 }
-}
+
+if (size <= 0)
+_putchar('\n');
 }
 

119bb278a86dd0a063dea0eadde51566fa88b3bd
Mzokhulayo <mzokhulayo.mdubeki@hotmail.com>
2023-08-26 04:03:56 +0200

    104-print_buffer.c

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,45 +1,58 @@
 #include "main.h"
 #include <stdio.h>
-
 /**
- * print_buffer - Prints the content of a buffer in a formatted way.
- * @b: The input buffer to be printed.
- * @size: The size of the buffer.
+ * print_line - prints a s bytes of a buffer
+ * @c: buffer to print
+ * @s: bytes of buffer to print
+ * @l: line of buffer to print
+ *
+ * Return: void
  */
-void print_buffer(char *b, int size)
-{
-int i, j;
 
-for (i = 0; i < size; i += 10)
+void print_line(char *c, int s, int l)
 {
-printf("%08x: ", i);
-
-for (j = 0; j < 10; j++)
+int j, k;
+for (j = 0; j <= 9; j++)
 {
-if (i + j < size)
-printf("%02x", (unsigned char)b[i + j]);
+if (j <= s)
+printf("%02x", c[l * 10 + j]);
 else
 printf("  ");
-
-if (j % 2 != 0)
-printf(" ");
+if (j % 2)
+putchar(' ');
 }
-
-for (j = 0; j < 10; j++)
+for (k = 0; k <= s; k++)
 {
-if (i + j < size)
-{
-char c = b[i + j];
-if (c >= 32 && c <= 126)
-putchar(c);
+if (c[l * 10 + k] > 31 && c[l * 10 + k] < 127)
+putchar(c[l * 10 + k]);
 else
 putchar('.');
 }
+}
+
+/**
+ * print_buffer - prints a buffer
+ * @b: buffer to print
+ * @size: size of buffer
+ *
+ * Return: void
+ */
+void print_buffer(char *b, int size)
+{
+int i;
+for (i = 0; i <= (size - 1) / 10 && size; i++)
+{
+printf("%08x: ", i * 10);
+if (i < size / 10)
+{
+print_line(b, 9, i);
+}
 else
-putchar(' ');
+{
+print_line(b, size % 10 - 1, i);
 }
-
-printf("\n");
+putchar('\n');
 }
+if (size == 0)
+putchar('\n');
 }
-
